f3dc7128fa2d5b608d7f54b47483a57a6b319c2c
duvanmonroy <duvanmonroy1127@gmail.com>
2023-09-02 16:54:59 -0500

    hice calculadora en js

---
+++ calculadora.js
@@ -0,0 +1,48 @@
+function calcular() {
+    var n1, n2, 
+    n1= Number(prompt("Ingresar numero 1 a sumar"));
+    n2 = Number(prompt("Ingresar numero 2 a sumar"));
+    suma =(n1+n2)
+   alert('la suma es: '+ suma);
+  }
+
+  function res() {
+    var n1, n2, 
+    n1= Number(prompt("Ingresar numero 1 a restar"));
+    n2 = Number(prompt("Ingresar numero 2 a restar"));
+    resta =(n1-n2)
+   alert('la resta es: '+ resta);  
+  }
+
+  function multi() {
+    var n1, n2, 
+    n1= Number(prompt("Ingresar numero 1 a multiplicar"));
+    n2 = Number(prompt("Ingresar numero 2 a multiplicar"));
+    multiplicar =(n1*n2)
+   alert('la multiplicacion es: '+ multiplicar);
+  }
+
+  function divi() {
+    var n1, n2, 
+    n1= Number(prompt("Ingresar numero 1 a dividir"));
+    n2 = Number(prompt("Ingresar numero 2 a dividir"));
+    division =(n1/n2)
+   alert('la division es: '+ division);
+  }
+
+  function promedio() {
+    var n1, n2, 
+    n1= Number(prompt("Ingresar numero 1 a promediar"));
+    n2 = Number(prompt("Ingresar numero 2 a promediar"));
+    promedio =(n1+n2)/2
+   alert('el promedio  es: '+ promedio);
+  }
+
+  function porcentaje() {
+    var n1, n2, 
+    n1= Number(prompt("Ingresar numero 1 de porcentaje"));
+    n2 = Number(prompt("Ingresar numero 2 de valor"));
+    porcentaje =(n1*n2)/100
+   alert('el promedio  es: '+ porcentaje);
+  } 
+
